6f65d979807f38bb5ce11e947a66009c77cbd69e
markopedu <69790644+markopedu@users.noreply.github.com>
2023-02-16 18:00:39 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Marko Poikkimäki on 2023-02-16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
